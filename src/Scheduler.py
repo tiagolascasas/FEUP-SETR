959159b590eb93f7002d4be31dde3a5e19cb228e
Tiago Lascasas dos Santos <up201503616@fe.up.pt>
2019-05-15 14:49:26 +0100

    Added task times

--- src/Scheduler.py
+++ src/Scheduler.py
@@ -1,6 +1,8 @@
 import sched, time
-from enum import Enum
-from pprint import pprint
+
+# Models periodic, sporadic and aperiodic tasks using
+# Python's generic EDF scheduler. See implementation:
+# https://github.com/python/cpython/blob/3.7/Lib/sched.py
 
 def create_scheduler():
     return sched.scheduler(time.monotonic, time.sleep)
@@ -8,22 +10,22 @@ def create_scheduler():
 def sched_periodic(scheduler, task):
     scheduler.enterabs(task.delta, task.priority, sched_sporadic,
                 (scheduler, task))
-    task.func(**task.args)
+    task.func(**task.kwargs)
 
 def sched_sporadic(scheduler, task):
     scheduler.enter(task.delta, task.priority, sched_sporadic,
                 (scheduler, task))
-    task.func(**task.args)
+    task.func(**task.kwargs)
 
 def sched_aperiodic(scheduler, task):
-    scheduler.enterabs(task.delta, task.priority, task.func, kwargs=task.args)
+    scheduler.enterabs(task.delta, task.priority, task.func, kwargs=task.kwargs)
 
 # Generic task for the scheduler
 class Task:
-    def __init__(self, delta, priority, func, args=()):
+    def __init__(self, delta, priority, func, kwargs={}):
         self.delta = delta / float(1000)
         self.priority = priority
         self.func = func
-        self.args = args
+        self.kwargs = kwargs
 
 
--- src/main.py
+++ src/main.py
@@ -1,38 +1,69 @@
-import Scheduler
+#!/usr/bin/env python
+import scheduler
+import signal
+import sys
 import time
+import functools
+
+task_times = {}
+times_record = {}
 
 def main():
+    # Init system
+    init()
+
     # Periodic
-    t1 = Scheduler.Task(1000, 1, task_debug, {'id': 't1'})
-    t3 = Scheduler.Task(1700, 1, task_debug, {'id': 't3'})
-    t6 = Scheduler.Task(2000, 1, task_debug, {'id': 't6'})
+    t1 = scheduler.Task(1000, 1, task_debug, {'id': 't1'})
+    t3 = scheduler.Task(1700, 1, task_debug, {'id': 't3'})
+    t6 = scheduler.Task(2000, 1, task_debug, {'id': 't6'})
     # Sporadic
-    t2 = Scheduler.Task(1500, 1, task_debug, {'id': 't2'})
-    t4 = Scheduler.Task(500, 1, task_debug, {'id': 't4'})
-    t5 = Scheduler.Task(100, 1, task_debug, {'id': 't5'})
+    t2 = scheduler.Task(1500, 1, task_debug, {'id': 't2'})
+    t4 = scheduler.Task(500, 1, task_debug, {'id': 't4'})
+    t5 = scheduler.Task(100, 1, task_debug, {'id': 't5'})
+    # Aperiodic
+    t7 = scheduler.Task(100, 1, task_one_shot, {'arg': 't7'})
     
-    s = Scheduler.create_scheduler()
+    s = scheduler.create_scheduler()
 
     # Schedule periodic
-    Scheduler.sched_periodic(s, t1)
-    Scheduler.sched_periodic(s, t3)
-    Scheduler.sched_periodic(s, t6)
+    scheduler.sched_periodic(s, t1)
+    scheduler.sched_periodic(s, t3)
+    scheduler.sched_periodic(s, t6)
     # Schedule sporadic
-    Scheduler.sched_sporadic(s, t2)
-    Scheduler.sched_sporadic(s, t4)
-    Scheduler.sched_sporadic(s, t5)
+    scheduler.sched_sporadic(s, t2)
+    scheduler.sched_sporadic(s, t4)
+    scheduler.sched_sporadic(s, t5)
+    # Schedule aperiodic
+    scheduler.sched_aperiodic(s, t7)
 
     s.run()
 
-task_times = {}
+def init():
+    signal.signal(signal.SIGINT, signal_handler)
 
-def task_debug(id, foo=None):
+def task_debug(id):
     if id not in task_times:
         task_times[id] = time.monotonic()
     else:
         d = time.monotonic() - task_times[id]
         task_times[id] = time.monotonic()
-        print("Task %s: %s" % (id, round(d * 1000)))   
+        print("Task %s: %s" % (id, round(d * 1000)))
+        if id not in times_record:
+            times_record[id] = []
+        times_record[id].append(round(d * 1000))
+
+def task_one_shot(arg):
+    print("Task %s" % arg)
+
+def print_avg_times():
+    print("here")
+    for task, times in times_record.items():
+        avg = functools.reduce(lambda t1, t2: t1 + t2, times) / len(times)
+        print("Task %s: avg time %s" % (task, round(avg)))
+
+def signal_handler(sig, frame):
+        print_avg_times()
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
